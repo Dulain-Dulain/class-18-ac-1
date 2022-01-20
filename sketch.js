263745d540d537bc805dca9863f82f7007825319
Dulain-Dulain <93309888+Dulain-Dulain@users.noreply.github.com>
2022-01-20 16:42:39 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var student 
+function setup() {
+  createCanvas(400,400);
+student=new Student("John",12,6)
+student.display()
+student1=new Student("Connor",12,6)
+student1.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
